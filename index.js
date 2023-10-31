01affecf91c2ae7afccf29d4e25a0b66b97b028b
Olga <minaeva9@gmail.com>
2023-10-30 23:10:50 -0400

    if/else statement add check if Nan

--- index.js
+++ index.js
@@ -1,16 +1,37 @@
+document.addEventListener("DOMContentLoaded", function () {
+  populateBoard(16);
 
+  let sizeBtn = document.getElementById("size");
+  sizeBtn.addEventListener("click", function () {
+    let size = boardSize();
+    populateBoard(size);
+  });
+});
 
 function populateBoard(size) {
   let board = document.querySelector(".board");
 
   board.style.gridTemplateColumns = `repeat(${size},1fr)`;
   board.style.gridTemplateRow = `repeat(${size}, 1fr)`;
+
   let numDivs = size * size;
+
   for (let i = 0; i < numDivs; i++) {
     let div = document.createElement("div");
-    div.style.backgroundColor = 'grey';
+    div.style.backgroundColor = "grey";
     board.insertAdjacentElement("beforeend", div);
   }
 }
 
-populateBoard(16);
+function boardSize() {
+  let userInput = prompt("Please enter size of the board");
+  let message = document.querySelector("p");
+  if (userInput === "" || isNaN(userInput)) {
+    message.textContent = "Please enter a number";
+  } else if (userInput < 0 || userInput > 100) {
+    message.textContent = "Please enter a number between 1 and 100";
+  } else {
+    message.textContent = "Start drawing!";
+    return userInput;
+  }
+}
