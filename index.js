06390216b415b494e33e50dda445890ecaa99792
Olga <minaeva9@gmail.com>
2023-10-30 21:49:34 -0400

    populateBoard function added, title added in html

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
+
+
 function populateBoard(size) {
   let board = document.querySelector(".board");
 
   board.style.gridTemplateColumns = `repeat(${size},1fr)`;
   board.style.gridTemplateRow = `repeat(${size}, 1fr)`;
-
-  for (let i = 0; i < size * size; i++) {
-    let square = document.createElement("div");
-    square.style.background = "blue";
-    board.appendChild(square);
+  let numDivs = size * size;
+  for (let i = 0; i < numDivs; i++) {
+    let div = document.createElement("div");
+    div.style.backgroundColor = 'grey';
+    board.insertAdjacentElement("beforeend", div);
   }
 }
 
-populateBoard(8);
-
-function changeSize(input) {
-  if (input >= 2 && input <= 32) {
-    populateBoard(input);
-  } else {
-    alert("too many squares");
-  }
-}
+populateBoard(16);
